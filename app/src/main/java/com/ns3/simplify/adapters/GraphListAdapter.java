2fcecf7ccb79a46901c1a479ccc362441dddadd9
suchox <suchandrim.sarkar@gmail.com>
2016-05-10 15:36:34 +0530

    Graph Bug solved

--- app/src/main/java/com/ns3/simplify/adapters/GraphListAdapter.java
+++ app/src/main/java/com/ns3/simplify/adapters/GraphListAdapter.java
@@ -88,10 +88,7 @@ public class GraphListAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        if(studentsList.size()%10 == 0)
-            return studentsList.size()/10;
-        else
-            return studentsList.size()/10 + 1;
+            return studentsList.size();
     }
 
     @Override
